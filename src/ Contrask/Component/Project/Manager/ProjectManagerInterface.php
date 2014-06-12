6905af400572d28c08760d14fa5c65075001c9b5
Yusliel Garcia <yuslielg@gmail.com>
2014-06-12 07:45:25 -0400

    Added methods pick & collect

--- src/ Contrask/Component/Project/Manager/ProjectManagerInterface.php
+++ src/ Contrask/Component/Project/Manager/ProjectManagerInterface.php
@@ -7,5 +7,20 @@ namespace Contrask\Component\Project\Manager;
  */
 interface ProjectManagerInterface
 {
-   
+    /**
+     * Return the project by given criteria.
+     *
+     * @param $criteria
+     * @return \Contrask\Component\Project\Entity\ProjectInterface The project
+     */
+    public function pick($criteria);
+
+    /**
+     * Return the projects by given criteria.
+     * It returns all projects if criteria is null
+     *
+     * @param mixed $criteria
+     * @return array The projects
+     */
+    public function collect($criteria = null);
 }
